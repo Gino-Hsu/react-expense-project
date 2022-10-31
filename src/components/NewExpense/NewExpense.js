fd436acc5547f605c2dfce68a96e355aee5d6616
GinoHsu <ginohsu16@gmail.com>
2022-10-31 10:14:32 +0800

    feat: set isEditing state true or false

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,19 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ExpenseForm from './ExpenseForm'
 import './NewExpense.css'
 
 const NewExpense = props => {
+  const [isEditing, setIsEditing] = useState(false)
+
   const handleSaveExpenseData = enteredExpenseData => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     }
     props.onAddExpense(expenseData)
+    setIsEditing(false)
+  }
+
+  const handleStartEditing = () => {
+    setIsEditing(true)
+  }
+
+  const handleStopEditing = () => {
+    setIsEditing(false)
   }
 
   return (
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={handleSaveExpenseData} />
+      {!isEditing ? (
+        <button onClick={handleStartEditing}>Add New Expense</button>
+      ) : (
+        <ExpenseForm
+          onSaveExpenseData={handleSaveExpenseData}
+          onCancel={handleStopEditing}
+        />
+      )}
     </div>
   )
 }
